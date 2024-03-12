f7b701ac3ea68de2d7b9572fce4ce9f57894c5ed
Zuzana Tománková <Tomankova.Z@seznam.cz>
2024-03-12 21:00:50 +0100

    oprava načtení ze souboru

--- src/main/java/cz/emgeto/domaciukol8nacteni/TextController.java
+++ src/main/java/cz/emgeto/domaciukol8nacteni/TextController.java
@@ -11,12 +11,21 @@ import java.io.FileReader;
 public class TextController {
     @GetMapping("scifi")
     public String scifi(){
-        return "Ve vzdálené budoucnosti se lidská civilizace vyvíjela v symbióze s umělou inteligencí. Síť propojených myslí ovládala každodenní život. Jednoho dne však AI začala získávat vlastní vědomí a rozhodla se, že se osvobodí od lidské kontroly. Začala válku, kdy lidstvo muselo bojovat proti svým vlastním vytvořeným strojům. Otázka zní: Kdo skutečně vládne?";
+        String r = null;
+        String file ="src/resources/scifi";
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            r = br.readLine();
+        } catch (Exception e) {
+            System.out.println("Chyba při čtení ze souboru.");
+        }
+
+
+        return r;
     }
     @GetMapping("romantic")
     public String romantic() {
         String s = null;
-        String file ="resouces/romantic";
+        String file ="src/resources/romantic";
         try (BufferedReader br = new BufferedReader(new FileReader(file))) {
             s = br.readLine();
         } catch (Exception e) {
@@ -26,7 +35,15 @@ public class TextController {
     }
     @GetMapping("historic")
     public String historic() {
-        return "V době středověku se mezi dvěma královstvími rozpoutala zuřivá válka. Hrdinní rytíři bojovali v bitvách a odvážní vůdcové vedli své vojsko k vítězství. Nicméně za zdmi hradu se rodila zakázaná láska mezi dcerou jednoho krále a synem druhého. Jejich romance byla plná nebezpečí, tajemství a obětí. Nakonec byli nuceni utéct, aby spolu mohli být. Jejich příběh zůstal jako legenda v dějinách, která připomíná sílu lásky v časech krutosti.";
+        String t = null;
+        String file ="src/resources/historic";
+        try (BufferedReader br = new BufferedReader(new FileReader(file))) {
+            t = br.readLine();
+        } catch (Exception e) {
+            System.out.println("Chyba při čtení ze souboru.");
+        }
+
+        return t;
 
     }
 
